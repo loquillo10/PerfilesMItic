10b4ce5254c54ee5782f771c4b465b9f1aefd9e8
Pedro Gonzalez <viento100@gmail.com>
2021-07-23 08:00:37 -0400

    Migracion de Hijos

--- PerfilesMItic/Migrations/20210723115957_Update-Model-Hijos.Designer.cs
+++ PerfilesMItic/Migrations/20210723115957_Update-Model-Hijos.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PerfilesMItic.Data;
 
 namespace PerfilesMItic.Migrations
 {
     [DbContext(typeof(PerfilesMIticContext))]
-    partial class PerfilesMIticContextModelSnapshot : ModelSnapshot
+    [Migration("20210723115957_Update-Model-Hijos")]
+    partial class UpdateModelHijos
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
